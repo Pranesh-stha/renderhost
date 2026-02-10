49ff3c2eed865634d10a360f95b83c7fd09ebb86
Your Name <pranesh15shrestha@gmail.com>
2026-02-10 11:03:02 +0545

    fixed the lint issue

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 import express from 'express';
-import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// app.use(cors({
-//   origin: 'http://localhost:3000',
-//   methods: ['GET','POST','PUT','DELETE'],
-//   credentials: true
-// }));
+
 
 app.get('/hi', (req, res) => {
   res.status(200).json('Hi from Express server 👋 v2');
