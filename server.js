d69033ff0e8d260f04224217fe26409831d7cfa7
Your Name <pranesh15shrestha@gmail.com>
2026-02-10 09:10:18 +0545

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// app.use(cors({
+//   origin: 'http://localhost:3000',
+//   methods: ['GET','POST','PUT','DELETE'],
+//   credentials: true
+// }));
+
+app.get('/hi', (req, res) => {
+  res.status(200).json('Hi from Express server 👋');
+});
+
+app.get('/pranesh', (req, res) => {
+  res.status(200).json('Hi from pranesh home server 👋');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
